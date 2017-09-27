c4b979de5b22a3ad208a8535244892383e83c821
theCydonian <31706764+theCydonian@users.noreply.github.com>
2017-09-27 11:32:16 -0700

    doc setup for BinaryTreeUtility

---
+++ src/structures/BinaryTreeUtilityI.java
@@ -0,0 +1,43 @@
+package structures;
+
+import java.util.Iterator;
+
+public class BinaryTreeUtilityI implements BinaryTreeUtility{
+
+	@Override
+	public <T> Iterator<T> getPreOrderIterator(BinaryTreeNode<T> root) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <T> Iterator<T> getInOrderIterator(BinaryTreeNode<T> root) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <T> Iterator<T> getPostOrderIterator(BinaryTreeNode<T> root) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <T> int getDepth(BinaryTreeNode<T> root) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public <T> boolean isBalanced(BinaryTreeNode<T> root, int tolerance) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public <T extends Comparable<? super T>> boolean isBST(BinaryTreeNode<T> root) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
