93df5596d96d1177d6213f9d73a229b973f9a044
theCydonian <31706764+theCydonian@users.noreply.github.com>
2017-10-11 11:39:42 -0700

    added setup for BST

---
+++ src/structures/BinarySearchTreeI.java
@@ -0,0 +1,62 @@
+package structures;
+
+import java.util.Iterator;
+
+public class BinarySearchTreeI<T extends Comparable<? super T>> implements BinarySearchTree<T>{
+
+	@Override
+	public BinarySearchTree<T> add(T toAdd) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean contains(T toFind) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean remove(T toRemove) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public T getMinimum() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public T getMaximum() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public BinaryTreeNode<T> toBinaryTreeNode() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Iterator<T> iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+}
